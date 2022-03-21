23e9e2d14c003febc411e192efb0522af69ec90e
Robin Hallerbach <robinhallerbach13@googlemail.com>
2022-03-21 16:10:56 +0100

    Add files via upload

--- jssql.js
+++ jssql.js
@@ -10,266 +10,171 @@ class JsSql {
         }
         return row;
     }
+    static #eval(fieldValue,operator, value) {
+        switch (operator) {
+            case '=':
+                return fieldValue == value;
+            case '!=':
+                return fieldValue != value;
+            case '>':
+                return fieldValue > value;
+            case '<':
+                return fieldValue < value;
+            case '>=':
+                return fieldValue >= value;
+            case '<=':
+                return fieldValue <= value;
+            case 'LIKE':
+                let parts = value.split('%');
+                parts = parts.map((part) => {
+                    return part.replace(/_/g, '.?');
+                });
+                let regex = new RegExp(parts.join('.*'));
+                if (!regex.test(fieldValue)) {
+                    return false;
+                }
+                return true;
+            case 'IN':
+                return value.includes(fieldValue);
+            case 'NOT IN':
+                return !value.includes(fieldValue);
+            case 'CONTAINS':
+                return fieldValue.includes(value);
+            case 'NOT CONTAINS':
+                return !fieldValue.includes(value);
+            default:
+                throw new Error(`Unknown operator: ${operator}`);
+        }
+    }
+
+    static #checkRow(row, where){
+        let left, right;
+        if(where.left.type === 'column_ref'){
+            left = this.#getData(row,where.left.column)
+        }else if(where.left.type === 'binary_expr'){
+            left = this.#checkRow(row,where.left)
+        }else{
+            left = where.left.value
+        }
+        if(where.right.type === 'column_ref'){
+            right = this.#getData(row,where.right.column)
+        }else if(where.right.type === 'binary_expr'){
+            right = this.#checkRow(row,where.right)
+        }else{
+            right = where.right.value
+        }
+        return this.#eval(left,where.operator,right)
+
+    }
     static run(db, query, config = {}) {
         let { keepEmptyCells = true } = config;
-        let keyWords = [
-            'SELECT ',
-            ' FROM ',
-            ' WHERE ',
-            ' AND ',
-            ' OR ',
-            ' ORDER BY ',
-            ' ASC',
-            ' DESC',
-            ' LIMIT ',
-            ' OFFSET ',
- 
-            ' GROUP BY ',
-            ' HAVING ',
-            ' JOIN '
-        ];
-        return query
-            .split('UNION')
-            .map((q) => {
-                let keywordArray = q
-                    .toUpperCase()
-                    .match(new RegExp(keyWords.join('|'), 'g'));
-                let dataArray = q.split(
-                    new RegExp(`${keyWords.join('|')}`, 'g')
-                );
-                dataArray.shift();
-                let parsedSql = {
-                    select: '',
-                    from: '',
-                    where: [],
-                    orderBy: ['', 'ASC'],
-                    limit: '',
-                    offset: '',
-                    join: ''
-                };
-                keywordArray.forEach((keyword, i) => {
-                    keyword = keyword.trim();
-                    if (keyword === 'SELECT') {
-                        parsedSql.select = dataArray[i].trim();
-                    }
-                    if (keyword === 'FROM') {
-                        parsedSql.from = dataArray[i].trim();
-                    }
-                    if (['WHERE', 'AND', 'OR'].includes(keyword)) {
-                        parsedSql.where.push(dataArray[i].trim());
-                    }
-                    if (keyword === 'ORDER BY') {
-                        parsedSql.orderBy[0] = dataArray[i].trim();
-                    }
-                    if (keyword === 'ASC' || keyword === 'DESC') {
-                        parsedSql.orderBy[1] = keyword;
-                    }
-                    if (keyword === 'LIMIT') {
-                        parsedSql.limit = dataArray[i].trim();
-                    }
-                    if (keyword === 'OFFSET') {
-                        parsedSql.offset = dataArray[i].trim();
-                    }
-                    if (keyword === 'GROUP BY') {
-                        parsedSql.groupBy = dataArray[i].trim();
-                    }
-                    if (keyword === 'JOIN') {
-                        parsedSql.join = dataArray[i].trim();
-                    }
-                });
-                let table = parsedSql.from;
-                let data = db[table];
-                if (parsedSql.join) {
-                    data = data.map((row) => {
-                        return { [table]: row };
-                    });
+        const parser = new NodeSQLParser.Parser()
+        const ast = parser.parse(query).ast
+        let table = ast.from[0].table;
+        let data = db[table];
+        /* if (parsedSql.join) {
+            data = data.map((row) => {
+                return { [table]: row };
+            });
 
-                    let [joinTable, joinCond] = parsedSql.join
-                        .split('ON')
-                        .map((s) => s.trim());
-                    let joinTableData = db[joinTable];
-                    let joinCondArray = joinCond
-                        .split('=')
-                        .map((c) => c.trim());
-                    data.forEach((row) => {
-                        let leftRow = row[table];
-                        let left = JsSql.#getData(leftRow, joinCondArray[0]);
-                        if (left) {
-                            let right = joinTableData.find((r) => {
-                                return r[joinCondArray[1]] === left;
-                            });
-                            if (right) {
-                                row[joinTable] = right;
-                            }
-                        }
+            let [joinTable, joinCond] = parsedSql.join
+                .split('ON')
+                .map((s) => s.trim());
+            let joinTableData = db[joinTable];
+            let joinCondArray = joinCond
+                .split('=')
+                .map((c) => c.trim());
+            data.forEach((row) => {
+                let leftRow = row[table];
+                let left = JsSql.#getData(leftRow, joinCondArray[0]);
+                if (left) {
+                    let right = joinTableData.find((r) => {
+                        return r[joinCondArray[1]] === left;
                     });
+                    if (right) {
+                        row[joinTable] = right;
+                    }
                 }
-                let fields = parsedSql.select.split(',');
-                if (parsedSql.select === '*') {
-                    fields = Array.from(
-                        data.reduce((acc, item) => {
-                            Object.keys(item).forEach((key) => {
-                                acc.add(key);
-                            });
-                            return acc;
-                        }, new Set())
-                    );
-                }
-
-                let where = parsedSql.where.map((whereItem) => {
-                    let [field, operator, ...value] = whereItem.split(/\s+/);
-                    return { field, operator, value: value.join(' ') };
-                });
-                let result = [];
-
-                result = data.filter((item) => {
-                    let isValid = true;
-                    where.forEach((whereItem) => {
-                        let field = whereItem.field;
-                        let operator = whereItem.operator;
-                        let value = whereItem.value.trim();
-                        if (value.match(/'|".*'|"/)) {
-                            value = value.replace(/'|"/g, '');
-                        } else if (value.match(/\d+/)) {
-                            value = parseInt(value);
-                        } else {
-                            value = JsSql.#getData(item, value);
-                        }
-                        let fieldValue = JsSql.#getData(item, field);
-                        if (operator === '=') {
-                            if (fieldValue != value) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === '!=') {
-                            if (fieldValue == value) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === '>') {
-                            if (fieldValue <= value) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === '<') {
-                            if (fieldValue >= value) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === '>=') {
-                            if (fieldValue < value) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === '<=') {
-                            if (fieldValue > value) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === 'LIKE') {
-                            let parts = value.split('%');
-                            parts = parts.map((part) => {
-                                return part.replace(/_/g, '.?');
-                            });
-                            let regex = new RegExp(parts.join('.*'));
-                            if (!regex.test(fieldValue)) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === 'NOT LIKE') {
-                            let parts = value.split('%');
-                            parts = parts.map((part) => {
-                                return part.replace(/_/g, '.?');
-                            });
-                            let regex = new RegExp(parts.join('.*'));
-                            if (regex.test(fieldValue)) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === 'IN') {
-                            if (!value.split(',').includes(fieldValue)) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === 'NOT IN') {
-                            if (value.split(',').includes(fieldValue)) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === 'CONTAINS') {
-                            if (!fieldValue.includes(value)) {
-                                isValid = false;
-                            }
-                        }
-                        if (operator === 'NOT CONTAINS') {
-                            if (fieldValue.includes(value)) {
-                                isValid = false;
-                            }
-                        }
+            });
+        } */
+        let fields = ast.columns === '*' ? '*' : ast.columns.map((c) => c.expr.column);
+        if (ast.columns === '*' ) {
+            fields = Array.from(
+                data.reduce((acc, item) => {
+                    Object.keys(item).forEach((key) => {
+                        acc.add(key);
                     });
-                    return isValid;
-                });
-                result = result.map((r,i)=>({rownum:i+1,...r}));
-                result = result.reduce((acc, item) => {
-                    let newItem = {};
-                    fields.forEach((field) => {
-                        let fields = field
-                            .split('AS')
-                            .map((item) => item.trim());
-                        let as = (fields[1] || field).trim();
-                        let concat = fields[0]
-                            .split('||')
-                            .map((item) => item.trim());
-                        if (concat.length > 1) {
-                            newItem[as] = concat.reduce((acc, val) => {
-                                if (val.match(/'|"/)) {
-                                    return acc + val.replace(/'|"/g, '');
-                                } else {
-                                    return (
-                                        acc + JsSql.#getData(item, val) || ''
-                                    );
-                                }
-                            }, '');
-                        } else {
-                            if (concat[0].match(/'|"/)) {
-                                newItem[as] = concat[0].replace(/'|"/g, '');
-                            } else {
-                                newItem[as] =
-                                    JsSql.#getData(item, concat[0]) || '';
-                                if (!keepEmptyCells && newItem[as] === '') {
-                                    delete newItem[as];
-                                }
-                            }
-                        }
-                    });
-                    acc.push(newItem);
                     return acc;
-                }, []);
-                if (parsedSql.orderBy[0]) {
-                    result.sort((a, b) => {
-                        if (parsedSql.orderBy[1] === 'ASC') {
-                            return JsSql.#getData(a, parsedSql.orderBy[0]) >
-                                JsSql.#getData(b, parsedSql.orderBy[0])
-                                ? 1
-                                : -1;
+                }, new Set())
+            );
+        }
+ 
+        let result = [];
+        console.log(ast.where)
+        result = data.filter((item) => {
+            return JsSql.#checkRow(item, ast.where);
+        });
+        result = result.map((r,i)=>({rownum:i+1,...r}));
+        result = result.reduce((acc, item) => {
+            let newItem = {};
+            fields.forEach((field) => {
+                let fields = field
+                    .split('AS')
+                    .map((item) => item.trim());
+                let as = (fields[1] || field).trim();
+                let concat = fields[0]
+                    .split('||')
+                    .map((item) => item.trim());
+                if (concat.length > 1) {
+                    newItem[as] = concat.reduce((acc, val) => {
+                        if (val.match(/'|"/)) {
+                            return acc + val.replace(/'|"/g, '');
+                        } else {
+                            return (
+                                acc + JsSql.#getData(item, val) || ''
+                            );
                         }
-                        if (parsedSql.orderBy[1] === 'DESC') {
-                            return JsSql.#getData(a, parsedSql.orderBy[0]) >
-                                JsSql.#getData(b, parsedSql.orderBy[0])
-                                ? -1
-                                : 1;
+                    }, '');
+                } else {
+                    if (concat[0].match(/'|"/)) {
+                        newItem[as] = concat[0].replace(/'|"/g, '');
+                    } else {
+                        newItem[as] =
+                            JsSql.#getData(item, concat[0]) || '';
+                        if (!keepEmptyCells && newItem[as] === '') {
+                            delete newItem[as];
                         }
-                    });
+                    }
                 }
-                if (parsedSql.offset) {
-                    result = result.slice(parsedSql.offset);
+            });
+            acc.push(newItem);
+            return acc;
+        }, []);
+        if (ast.orderby) {
+            result.sort((a, b) => {
+                if (ast.orderby[0].type === 'ASC') {
+                    return JsSql.#getData(a, ast.orderby[0].expr.column) >
+                        JsSql.#getData(b, ast.orderby[0].expr.column)
+                        ? 1
+                        : -1;
                 }
-                if (parsedSql.limit) {
-                    result = result.slice(0, parsedSql.limit);
+                if (ast.orderby[0].type  === 'DESC') {
+                    return JsSql.#getData(a, ast.orderby[0].expr.column) >
+                        JsSql.#getData(b, ast.orderby[0].expr.column)
+                        ? -1
+                        : 1;
                 }
-                return result;
-            })
-            .flat();
+            });
+        }
+        /* if (parsedSql.offset) {
+            result = result.slice(parsedSql.offset);
+        } */
+        if (ast.limit) {
+            result = result.slice(0, ast.limit.value[0].value);
+        }
+        console.log(result);
+        return result;
+          
     }
     run(query, config = {}) {
         return JsSql.run(this.data, query, config);
